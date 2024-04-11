898f4c6a48f85d15f7ebe2e265d3c290e8a76b08
azimochilov <azimochilov226@gmail.com>
2024-04-11 18:45:54 +0500

    intial commit

---
+++ Musician-Portfolio/src/main/java/Musician/Portfolio/Musician/Portfolio/MusicianPortfolioApplication.java
@@ -0,0 +1,13 @@
+package Musician.Portfolio.Musician.Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicianPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicianPortfolioApplication.class, args);
+	}
+
+}
---
+++ Musician-Portfolio/src/test/java/Musician/Portfolio/Musician/Portfolio/MusicianPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package Musician.Portfolio.Musician.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicianPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
