6b3a0d2f574d8f579039e5b5419a1929a1b559fe
azimochilov <azimochilov226@gmail.com>
2024-04-30 21:05:40 +0500

    changed 4

---
+++ Musician-Portfolio/src/main/java/Musician/Portfolio/Musician/Portfolio/api/test/TestController.java
@@ -0,0 +1,25 @@
+package Musician.Portfolio.Musician.Portfolio.api.test;
+
+import Musician.Portfolio.Musician.Portfolio.domain.entities.contact.Contact;
+import Musician.Portfolio.Musician.Portfolio.service.contact.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tt")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TestController {
+
+    private final ContactService contactService;
+
+    @Autowired
+    public TestController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+    @PostMapping("/")
+    public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
+        Contact newContact = contactService.saveContact(contact);
+        return ResponseEntity.ok(newContact);
+    }
+}
