81b30eab07e646b6c5476157d0468aaba6ca3b7f
azimochilov <azimochilov226@gmail.com>
2024-04-30 21:34:52 +0500

    changed 5

--- Musician-Portfolio/src/main/java/Musician/Portfolio/Musician/Portfolio/config/WebConfig.java
+++ Musician-Portfolio/src/main/java/Musician/Portfolio/Musician/Portfolio/config/WebConfig.java
@@ -2,19 +2,26 @@ package Musician.Portfolio.Musician.Portfolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Allows credentials
+        config.addAllowedOriginPattern("http://54.174.103.74:8080"); // Use actual allowed domain(s)
+        config.addAllowedOriginPattern("http://54.174.103.74:8080"); // Example for allowing subdomains
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
 
